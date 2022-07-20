cfd9caba3833a2b03c116870bad997874df44c81
Justin Dority <justindority@gmail.com>
2022-07-20 11:27:45 -0500

    added pet click event

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,13 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
@@ -14,3 +15,23 @@ export const RegisteredPets = () => {
     return petHTML
 }
 
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pet")) {
+            const [,petID] = itemClicked.id.split("--")
+
+            for (const pet of pets) {
+                if (pet.id === parseInt(petID)) {
+                    for(const walker of walkers){
+                        if(pet.walkerId === walker.id){
+                            window.alert(`${pet.name} is being walked by ${walker.name}.`)
+                        }
+                    }
+                }
+            }
+        }
+    }
+)
